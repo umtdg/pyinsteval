74e5d3b35cdd84ab4c7da490a6e8c79d16012e02
Umut Dag <umu7d9@gmail.com>
2022-10-30 16:35:38 +0300

    initial commit
    Signed-off-by: Umut Dag <umu7d9@gmail.com>

---
+++ rank.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+
+import argparse
+import difflib
+import glob
+import os
+import pandas
+import pathlib
+import requests
+import rich
+import sys
+
+from typing import Dict, List
+
+
+# Globals
+COL_COURSE_CODE = "Course Code"
+COL_SECTION_GPA = "Section GPA"
+COL_COURSE_GPA = "Course CPA*"
+
+CWD = pathlib.Path(os.path.dirname(os.path.relpath(__file__)))
+INSTRUCTOR_DIR = CWD.joinpath(".instructors")
+TEMP_DIR = pathlib.Path("/tmp")
+
+
+# Functions
+def log(msg: str, level: str, color: str, style: str = "bold"):
+    rich.print(f"[{style} {color}][{level}][/{style} {color}] {msg}")
+
+
+def info(msg: str):
+    log(msg, "info", "blue")
+
+
+def warn(msg: str):
+    log(msg, "warn", "yellow")
+
+
+def error(msg: str):
+    log(msg, "error", "error")
+
+
+def title(s: str):
+    rich.print(f"[bold magenta]{s}[/bold magenta]")
+
+
+def report(report_title: str, result):
+    rich.print(f"[bold green]{report_title}:[/bold green] {result}")
+
+
+class Instructor:
+    def __init__(self, name: str, url: str):
+        self.name = name
+        self.url = url
+
+
+class Report:
+    def __init__(
+            self,
+            weight_of_section_gpa_gt_course_avg: float,
+            weight_of_section_gpa_gt_3: float,
+            weight_of_course_gpa_gt_3: float,
+            avg_section_course_diff: float,
+            instructor: Instructor
+    ):
+        self.weight_of_section_gpa_gt_course_avg = weight_of_section_gpa_gt_course_avg
+        self.weight_of_section_gpa_gt_3 = weight_of_section_gpa_gt_3
+        self.weight_of_course_gpa_gt_3 = weight_of_course_gpa_gt_3
+        self.avg_section_course_diff = avg_section_course_diff
+        self.instructor: Instructor = instructor
+
+    def __lt__(self, other: "Report"):
+        if self.avg_section_course_diff == other.avg_section_course_diff:
+            return self.weight_of_section_gpa_gt_course_avg < other.weight_of_section_gpa_gt_course_avg
+
+        return self.avg_section_course_diff < other.avg_section_course_diff
+
+    def __eq__(self, other: "Report"):
+        return self.avg_section_course_diff == other.avg_section_course_diff \
+                and self.weight_of_section_gpa_gt_course_avg == other.weight_of_section_gpa_gt_course_avg
+
+    def print(self, names_only: bool = False):
+        if names_only:
+            title(f"{self.instructor.name}")
+            return
+
+        title(f"{self.instructor.name}:")
+        report("Weight of Section GPA > Course Avg", f"{self.weight_of_section_gpa_gt_course_avg:.2f}%")
+        report("Weight of Section GPA > 3", f"{self.weight_of_section_gpa_gt_3:.2f}%")
+        report("Weight of Course GPA > 3", f"{self.weight_of_course_gpa_gt_3:.2f}%")
+        report("Average of Section GPA - Course Average", f"{self.avg_section_course_diff:.2f}")
+        print()
+
+
+def download_file(url: str, _decode: bool = False, _encoding: str = "utf-8") -> bytes | str | None:
+    response = requests.get(url)
+    if not response.ok:
+        return None
+
+    if _decode:
+        return response.content.decode(_encoding)
+    else:
+        return response.content
+
+
+def fetch_evaluation_data(instructor: Instructor, override: bool = False) -> bool:
+    html_file = f"{instructor.name}.csv"
+    instructor_data_file = INSTRUCTOR_DIR.joinpath(html_file)
+
+    info(f"Fetching evaluation data for {instructor.name}")
+
+    if os.path.exists(instructor_data_file) and not override:
+        warn(f"Evaluation data for {instructor.name} already exists, skipping")
+        return True
+
+    html_data = download_file(instructor.url, _decode=True)
+    if html_data is None:
+        error(f"Could not download evaluation data from: {instructor.url}")
+        return False
+
+    instructor_data_file.parent.mkdir(parents=True, exist_ok=True)
+
+    data = pandas.read_html(html_data)[0]
+    data.to_csv(path_or_buf=instructor_data_file, index=False, sep="|",
+                                          columns=(COL_COURSE_CODE, COL_SECTION_GPA, COL_COURSE_GPA))
+
+    return True
+
+
+def load_instructors_from_list(filename: str) -> List[Instructor]:
+    instructors: List[Instructor] = []
+    with open(filename, "r") as f:
+        for line in f:
+            name, url = tuple(line.strip().split(";"))
+            instructors.append(Instructor(name, url))
+
+    return instructors
+
+
+def load_instructors_from_existing() -> Dict[Instructor, pandas.DataFrame]:
+    if not os.path.exists(INSTRUCTOR_DIR) or not os.path.isdir(INSTRUCTOR_DIR):
+        error(f"Folder {INSTRUCTOR_DIR} does not exists or is a file")
+        return {}
+
+    pattern = fr"{INSTRUCTOR_DIR}/*.csv"
+    file_list = [pathlib.Path(file) for file in glob.glob(pattern)]
+
+    instructors: Dict[Instructor, pandas.DataFrame] = {}
+    for file in file_list:
+        data = pandas.read_csv(filepath_or_buffer=file, sep="|")
+        instructor = Instructor(file.stem, data)
+        instructors[instructor] = data
+
+    return instructors
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", dest="instructor_list", required=False, default=None,
+                        help="File containing InstructorName;EvaluationUrl pair")
+    parser.add_argument("-c", "--course", dest="course", required=True,
+                        help="Course to grep from instructor evaluations")
+    parser.add_argument("-o", "--override", dest="override", required=False, default=False, action="store_true",
+                        help="Override existing instructor evaluation data")
+    parser.add_argument("-n", "--names", dest="names_only", required=False, default=False, action="store_true",
+                        help="Only show names")
+
+    return parser.parse_args(sys.argv[1:])
+
+
+def main():
+    args = parse_args()
+
+    if args.instructor_list is not None:
+        info("Instructor file is specified. Will try to fetch data for all and save them")
+
+        keys = load_instructors_from_list(args.instructor_list)
+        for key in keys:
+            fetch_evaluation_data(key, args.override)
+
+    instructors: Dict[Instructor, pandas.DataFrame] = load_instructors_from_existing()
+    instructor_reports: List[Report] = []
+
+    for instructor, data in instructors.items():
+        course_data = data[data[COL_COURSE_CODE] == args.course]
+        if len(course_data) == 0:
+            continue
+
+        section_gpas: pandas.Series = course_data[COL_SECTION_GPA]
+        course_gpas: pandas.Series = course_data[COL_COURSE_GPA]
+
+        section_gpa_gt_course: pandas.Series = section_gpas[section_gpas > course_gpas]
+        section_gpa_gt_3: pandas.Series = section_gpas[section_gpas > 3]
+        course_gpa_gt_3: pandas.Series = course_gpas[course_gpas > 3]
+        section_course_diff: pandas.Series = section_gpas - course_gpas
+
+        weight_of_section_gpa_gt_course_avg = len(section_gpa_gt_course)*100/len(course_data)
+        weight_of_section_gpa_gt_3 = len(section_gpa_gt_3)*100/len(course_data)
+        weight_of_course_gpa_gt_3 = len(course_gpa_gt_3)*100/len(course_data)
+        avg_section_course_diff = section_course_diff.mean()
+
+        rep = Report(weight_of_section_gpa_gt_course_avg,
+                     weight_of_section_gpa_gt_3,
+                     weight_of_course_gpa_gt_3,
+                     avg_section_course_diff,
+                     instructor)
+
+        instructor_reports.append(rep)
+
+    for rep in sorted(instructor_reports, reverse=True):
+        rep.print(args.names_only)
+
+
+if __name__ == "__main__":
+    main()
---
+++ watch_quota.py
@@ -0,0 +1,100 @@
+from argparse import ArgumentParser
+from datetime import datetime
+from sys import argv
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+parser = ArgumentParser()
+parser.add_argument("-c", "--course", dest="course", required=True)
+parser.add_argument("-s", "--section", nargs="+", dest="sections", required=True)
+parser.add_argument("-i", "--interval", type=int, dest="interval", required=False, default=60)
+
+args = parser.parse_args(argv[1:])
+
+interval = args.interval
+course = args.course
+sections = [section.zfill(3) for section in args.sections]
+section_quota_cache = {}
+url = f"https://stars.bilkent.edu.tr/homepage/offerings.php?COURSE_CODE={course.split(' ')[0]}"
+
+
+print(f"Course: {course}")
+print(f"Sections: {sections}")
+
+driver_opts = Options()
+driver_opts.headless = True
+driver_opts.set_preference("detach", True)
+
+driver = webdriver.Firefox(options=driver_opts)
+driver.get(url)
+
+max_tries = 10
+tries = 0
+try:
+    while True:
+        course_table = None
+        while True:
+            try:
+                if tries == max_tries:
+                    print("Could not load page")
+                    driver.get(url)
+                    tries = 0
+
+                course_table = driver.find_element(By.ID, "courses")
+                break
+            except:
+                tries += 1
+                sleep(1)
+
+        try:
+            course_elem = WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.ID, course)))
+            row_course_code = course_elem.find_elements(By.TAG_NAME, "td")[0]
+            assert row_course_code.text == course
+            row_course_code.click()
+        except Exception as e:
+            print(f"Could not click {course}: {e}")
+            driver.close()
+            exit()
+
+        has_changed_quota = False
+        for section in sections:
+            course_section = f"{course}-{section}"
+
+            try:
+                section_row = driver.find_element(By.ID, course_section)
+            except:
+                print(f"Could not found section {course_section}")
+                driver.close()
+                exit()
+
+            section_cols = section_row.find_elements(By.TAG_NAME, "td")
+            assert section_cols[0].text == course_section
+
+            if section in section_quota_cache:
+                old_quota = section_quota_cache[section]
+            else:
+                old_quota = ""
+
+            section_quota_cache[section] = section_cols[-2].text
+
+            if old_quota != section_quota_cache[section]:
+                print("[{}] Available quota in {}: {}".format(
+                    datetime.now().strftime('%m.%d.%Y, %H:%M:%S'),
+                    course_section,
+                    section_quota_cache[section]
+                ))
+                has_changed_quota = True
+
+        if not has_changed_quota:
+            print(f"Quotas are unchanged in the interval of {interval} second(s)")
+
+        sleep(interval)
+        driver.refresh()
+        print("------------------------")
+finally:
+    driver.close()
